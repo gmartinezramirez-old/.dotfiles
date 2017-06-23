568799059bdfada69713c4fa6207bda1770dc628
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2017-06-23 19:58:19 -0400

    CHG: now change keyboard script use setxkbmap

--- change-keyboard.sh
+++ change-keyboard.sh
@@ -1,2 +1,7 @@
-xmodmap ~/dotfiles/.xmodmap
+#xmodmap ~/dotfiles/.xmodmap
+setxkbmap \
+    -model pc105 \
+    -layout latam \
+    -option caps:swapescape \
+    -option terminate:ctrl_alt_bksp
 xcape -e 'Super_L=Escape'
